e0df44edbe3ba7f9a01f88c00a801ed8feec7114
daruuuu <alfiandaruh@gmail.com>
2023-02-12 16:37:57 +0700

    cors

--- next.config.js
+++ next.config.js
@@ -4,24 +4,11 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  async headers() {
+  async rewrites() {
     return [
       {
-        // matching all API routes
-        source: "https://nextjs-ecommerce-six-psi.vercel.app/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
+        source: "/api/:path*",
+        destination: "https://nextjs-ecommerce-six-psi.vercel.app/:path*",
       },
     ];
   },
