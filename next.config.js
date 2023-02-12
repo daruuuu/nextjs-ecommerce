b16facc379b4418bc9fbc6db26c17b0e71580419
daruuuu <alfiandaruh@gmail.com>
2023-02-12 17:13:05 +0700

    cors issue

--- next.config.js
+++ next.config.js
@@ -4,14 +4,6 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "https://nextjs-ecommerce-six-psi.vercel.app/:path*",
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
