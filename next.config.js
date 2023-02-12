2c97b32d91259fcdf7aca8af2801ece64a09fb1e
daruuuu <alfiandaruh@gmail.com>
2023-02-12 17:23:45 +0700

    cors

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
+const securityHeaders = [
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+];
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
   },
 };
 
